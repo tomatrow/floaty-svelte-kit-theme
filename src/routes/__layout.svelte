00891161ea83d3e6f6ba067af939e0214b875ff7
AJ Caldwell <tomatrow@gmail.com>
2021-11-20 13:04:57 -0800

    Formatting

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,26 +4,30 @@
 
 <script lang="ts">
     import "../app.css"
-    import { Main, Primary, PrimaryCompact, Secondary, Navbar} from "$lib"
+    import { Main, Primary, PrimaryCompact, Secondary, Navbar } from "$lib"
     import { Menu, Cross } from "$lib/svgs"
     import { merge } from "$lib/_common"
 
     let open: boolean
 </script>
 
-<Navbar
-    class="bg-red-500 gap-4 p-4"
->
+<Navbar class="bg-red-500 gap-4 p-4">
     <a href="/" class="p-4">LOGO</a>
 
     <Primary class="mx-auto gap-4" {menu} megaClass="bg-blue-500 gap-4 p-4">
         <svelte:fragment slot="link" let:href let:label let:level let:compact>
-            <a {href} class={merge(
-                "bg-green-500",
-                level === 0 && "p-4",
-                level === 1 && (compact ? 'p-4 duration-200 hover:bg-gray-100 whitespace-nowrap' : 'font-black'),
-                level === 2 && compact && "hover:bg-gray-100 duration-200 p-4 whitespace-nowrap"
-            )}>{label}!</a>
+            <a
+                {href}
+                class={merge(
+                    "bg-green-500",
+                    level === 0 && "p-4",
+                    level === 1 &&
+                        (compact
+                            ? "p-4 duration-200 hover:bg-gray-100 whitespace-nowrap"
+                            : "font-black"),
+                    level === 2 && compact && "hover:bg-gray-100 duration-200 p-4 whitespace-nowrap"
+                )}>{label}!</a
+            >
         </svelte:fragment>
     </Primary>
 
@@ -34,14 +38,19 @@
 
 <Main>
     <slot />
-    <footer class="bg-red-600 text-white mt-auto flex flex-col gap-4 sm:gap-8 p-4 sm:flex-row items-center">
+    <footer
+        class="bg-red-600 text-white mt-auto flex flex-col gap-4 sm:gap-8 p-4 sm:flex-row items-center"
+    >
         <a href="/" class="p-8">LOGO</a>
         <Secondary {menu} class="self-stretch sm:mx-auto sm:max-w-lg sm:w-full">
             <svelte:fragment slot="link" let:href let:label let:level let:compact>
-                <a {href} class={merge(
-                    compact && (level === 0 ? "py-2" : "block p-1"),
-                    !compact && (level === 0 ? "font-bold p-2" : "px-2 py-1")
-                )}>{level}-{label}!</a>
+                <a
+                    {href}
+                    class={merge(
+                        compact && (level === 0 ? "py-2" : "block p-1"),
+                        !compact && (level === 0 ? "font-bold p-2" : "px-2 py-1")
+                    )}>{level}-{label}!</a
+                >
             </svelte:fragment>
         </Secondary>
         <span class="whitespace-pre">
@@ -51,7 +60,12 @@
 </Main>
 
 {#if open}
-    <PrimaryCompact {menu} bind:open class="text-red-700 bg-green-300 p-4" rootClass="bg-white bg-opacity-80 z-10">
+    <PrimaryCompact
+        {menu}
+        bind:open
+        class="text-red-700 bg-green-300 p-4"
+        rootClass="bg-white bg-opacity-80 z-10"
+    >
         <svelte:fragment slot="link" let:level let:label let:href>
             <a {href} class="hover:underline">{level}-{label}!</a>
         </svelte:fragment>
