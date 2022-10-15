2712c8af479b398408eb458a88ad29041689f667
AJ Caldwell <tomatrow@gmail.com>
2022-10-15 16:02:28 -0700

    use svelte-package

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,20 +2,17 @@ const mode = process.env.NODE_ENV
 const dev = mode === "development"
 
 const config = {
-	mode: "jit",
-	purge: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
 		extend: {}
 	},
-
-	plugins: [require("@tailwindcss/aspect-ratio")]
 }
 
 if (!dev) {
-	config.purge = ["./src/lib/**/*.{html,js,svelte,ts}"]
-	config.important = ".floaty-sveltekit"
+	config.content = ["./src/lib/**/*.{html,js,svelte,ts}"]
+	// config.important = ".floaty-sveltekit"
 	config.corePlugins = {
-		preflight: dev
+		preflight: false
 	}
 }
 
